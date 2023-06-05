f66c9fa27589f2b8f4b1b8b34c395ed3a05ce9a0
Kantharaj7349668197 <kantharajgowda4@gmail.com>
2023-06-05 11:07:55 +0530

    kantha

---
+++ src/test/java/practice1/class1test.java
@@ -0,0 +1,7 @@
+package practice1;
+
+public class class1test {
+public static void main(String[] args) {
+	System.out.println("kantha ");
+}
+}
---
+++ src/test/java/practice2/class2Test.java
@@ -0,0 +1,7 @@
+package practice2;
+
+public class class2Test {
+public static void main(String[] args) {
+	System.out.println("acchu");
+}
+}
